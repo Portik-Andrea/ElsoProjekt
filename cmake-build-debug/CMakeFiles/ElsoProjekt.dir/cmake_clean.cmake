565a695308d55aa68665a6e51c8ad1d9c228af8e
Portik-Andrea <portik.andrea@student.ms.sapientia.ro>
2021-03-11 18:49:32 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ElsoProjekt.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ElsoProjekt.dir/main.c.obj"
+  "ElsoProjekt.exe"
+  "ElsoProjekt.exe.manifest"
+  "ElsoProjekt.pdb"
+  "libElsoProjekt.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ElsoProjekt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

6fcfcc8da42796d5fb034079c11d159b90f82be2
Portik-Andrea <portik.andrea@student.ms.sapientia.ro>
2021-04-19 22:44:19 +0300

    Felhasznalok+Menu

--- userinterface.h
+++ userinterface.h
@@ -4,6 +4,13 @@
 
 #ifndef ELSOPROJEKT_USERINTERFACE_H
 #define ELSOPROJEKT_USERINTERFACE_H
+#include <stdio.h>
+#include <stdlib.h>
+#include "users.h"
+#include "tables.h"
+#include "cards.h"
+
+
 void start();
 
 void tableMeneger();

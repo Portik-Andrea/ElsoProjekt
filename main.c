365e5675d495a414a51f6f6e988092da24b94e67
Portik-Andrea <portik.andrea@student.ms.sapientia.ro>
2021-04-19 22:18:37 +0300

    Felhasznalok+Menu

--- main.c
+++ main.c
@@ -1,38 +1,7 @@
 #include <stdio.h>
-#include "cards.h"
-#include "users.h"
-#include "tables.h"
+#include "userinterface.h"
 
 int main() {
-    cards a;
-    a=create();
-    addUser(&a, 3);
-    setStatus(&a, 2);
-    printIds(a);
-
-    //valamiert ha nem csokkeno sorrendbe adom az ID-t nem jo a kiiras
-    //majd ra neznetek
-    USERS users;
-    createUsers(&users);
-    for(int i=0;i<3;++i){
-        char name[30];
-        printf("Felhasznalo neve:");
-        scanf("%s",name);
-        int ID;
-        printf("Azonositoszam:");
-        scanf("%i",&ID);
-        insertUser(&users,ID,name);
-    }
-    if(emplyUsers(users)){
-        printf("Nincs meg felfasznalo!\n");
-    }
-    else{
-        printf("Felfasznalo(k) letrejottek.\n");
-    }
-    printUsers(users);
-    Table *tablacska = createTable("Tabla");
-    addUserToTable(1, tablacska);
-    addCardToTable(1, tablacska);
-    printTable(tablacska);
+    start();
     return 0;
 }
